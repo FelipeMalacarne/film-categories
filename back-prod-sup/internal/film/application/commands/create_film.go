3ae7e007b7aaf29d6d0264a239545b25269d45a5
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-06-20 16:26:29 -0300

    create film command

---
+++ back-prod-sup/internal/film/application/commands/create_film.go
@@ -0,0 +1,31 @@
+package commands
+
+import (
+	"time"
+
+	"github.com/felipemalacarne/back-prod-sup/internal/film/domain"
+)
+
+type CreateFilmCommand struct {
+	ReleaseDate time.Time `json:"release_date"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Duration    uint16    `json:"duration"`
+}
+
+type CreateFilmHandler struct {
+	repository domain.FilmRepository
+}
+
+func NewCreateFilmHandler(repository domain.FilmRepository) *CreateFilmHandler {
+    return &CreateFilmHandler{repository}
+}
+
+func (h CreateFilmHandler) Handle(command CreateFilmCommand) (domain.Film, error) {
+    film := domain.NewFilm(command.Name, command.Description, command.Duration, command.ReleaseDate)
+    err := film.Validate()
+    if err != nil {
+        return domain.Film{}, err
+    }
+    return h.repository.Create(film)
+}
