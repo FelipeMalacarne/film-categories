f7bd2a35572955a4b86f1cc7c1aee21a84450b21
mateuszanella <mateusblk1@gmail.com>
2024-06-22 17:42:04 -0300

    Added base table

--- front-film-categories/src/App.tsx
+++ front-film-categories/src/App.tsx
@@ -1,36 +1,10 @@
 import { Navbar } from "./components/navbar";
 import { Outlet } from "react-router-dom";
-import { Button } from "./components/ui/button";
-import { useFilms } from "./hooks/films";
-import { useEffect } from "react";
 
 function App() {
-  const { films, createFilm, isLoading } = useFilms();
-
-  useEffect(() => {
-    console.log(films);
-  }, [films]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <>
       <main>
-        <Button
-          onClick={() =>
-            createFilm({
-              name: "New Film",
-              duration: 120,
-              description: "New Film Description",
-              author: "New Film Author",
-              release_date: new Date(),
-            })
-          }
-        >
-          Create Film
-        </Button>
         <Navbar />
         <Outlet />
       </main>
